093a50529995ad367c965494e5e2b65242b2678f
JohnieXu <281910378@qq.com>
2024-01-05 19:18:18 +0800

    feat: 添加 ikuuu 签到功能

---
+++ ikuuu/api.js
@@ -0,0 +1,27 @@
+const fetch = require('node-fetch');
+
+function checkin({ domain, cookie }) {
+  return new Promise((resolve, reject) => {
+    fetch(`https://${domain}/user/checkin`, {
+      headers: {
+        authority: domain,
+        referer: `https://${domain}/user`,
+        userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        origin: `https://${domain}`,
+        cookie
+      },
+      method: 'POST'
+    }).then(res => res.json)
+      .then(res => {
+        if (res.ret === '1') {
+          resolve(res.msg || JSON.stringify(res))
+        } else {
+          reject(new Error(res ? res.msg || JSON.stringify(res) : '未知错误'))
+        }
+      }).catch(reject);
+  })
+}
+
+module.exports = {
+  checkin
+}
