9d9a589e30a6b3fea009d793c16427707b55912b
JohnieXu <281910378@qq.com>
2024-12-11 15:44:14 +0800

    fix: fix type error

--- ikuuu/api.d.ts
+++ ikuuu/api.d.ts
@@ -1,8 +1,11 @@
-enum Ret {
+declare enum Ret {
   success = '1',
   fail = '0'
 }
 
 export interface Icheckin {
-  ({ domain: string, cookie: string }): Promise<string>
+  ({ domain, cookie }: {
+    domain: string,
+    cookie: string
+  }): Promise<string>
 }
