63b2179a7ab8e4b87a2d76322199dca9c97c46e6
JohnieXu <281910378@qq.com>
2025-09-25 15:26:35 +0800

    fix: email send error of hifini

--- src/hifini/index.ts
+++ src/hifini/index.ts
@@ -7,7 +7,9 @@ import { EmailOptions } from '../types/index.js';
 // const cookie = 'bbs_sid=qf95q4qkg12072pjevoe0eherv; bbs_token=xv35IdlO8Pac_2Bq4_2FfZsOrC4fMG9ABxozb5hrM_2B0toXnf4P1QFUcIXy5YWj1uVfciM0v0ZyxX14tMNiXfoiokrbqf_2FaZrYL1m';
 // const emailTo = '281910378@qq.com';
 
-const [cookie, emailTo, domain] = process.argv.slice(2)
+const [user, pass, emailTo, cookie, domain] = process.argv.slice(2)
+process.env.user = user; // 邮箱账号
+process.env.pass = pass; // 邮箱密码
 process.env.cookie = cookie
 process.env.emailTo = emailTo
 process.env.domain = domain
